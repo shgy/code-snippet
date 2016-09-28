e4b9992b3993ea1b7afd2e4914df6697dd937cef
shgy <810050504@qq.com>
2016-09-28 13:20:04 +0800

    add knowledge of getpid

--- syscall/unistd.getpid.c
+++ syscall/unistd.getpid.c
@@ -13,6 +13,8 @@
  * 
  *
  * 注意: getpid() 和 getppid() 函数的不用判断失败, 因为它们 "Always successfully returns process ID of caller"
+   getpid()系统调用返回当前进程的tgid值, 而不是pid的值. 理解这句话需要理解Linux线程相关的知识.
+   tgid表示 thread group id
  * */
 int main(int argc, char *argv[])
 {
